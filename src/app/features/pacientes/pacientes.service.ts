94d3e44107089b812ee84b0a1dde779ae5271fcc
maiajota <maia.joaovic@gmail.com>
2026-04-30 23:12:38 -0300

    atualizando os models de paciente

--- src/app/features/pacientes/pacientes.service.ts
+++ src/app/features/pacientes/pacientes.service.ts
@@ -1,29 +1,34 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { API_ROUTES } from '../../core/api-routes';
-import { Paciente } from './paciente.model';
+import {
+    PacienteAtualizar,
+    PacienteCadastro,
+    PacienteDetalhes,
+    PacienteResumo,
+} from './paciente.model';
 
 @Injectable({ providedIn: 'root' })
 export class PacientesService {
-  private http = inject(HttpClient);
+    private http = inject(HttpClient);
 
-  buscar() {
-    return this.http.get<Paciente[]>(API_ROUTES.pacientes.buscar);
-  }
+    buscar() {
+        return this.http.get<PacienteResumo[]>(API_ROUTES.pacientes.buscar);
+    }
 
-  buscarPorId(id: number) {
-    return this.http.get<Paciente>(API_ROUTES.pacientes.buscarPorId(id));
-  }
+    buscarPorId(id: number) {
+        return this.http.get<PacienteDetalhes>(API_ROUTES.pacientes.buscarPorId(id));
+    }
 
-  adicionar(paciente: Omit<Paciente, 'id'>) {
-    return this.http.post<Paciente>(API_ROUTES.pacientes.adicionar, paciente);
-  }
+    adicionar(paciente: Omit<PacienteCadastro, 'id'>) {
+        return this.http.post<PacienteCadastro>(API_ROUTES.pacientes.adicionar, paciente);
+    }
 
-  atualizar(id: number, paciente: Partial<Omit<Paciente, 'id'>>) {
-    return this.http.put<Paciente>(API_ROUTES.pacientes.atualizar(id), paciente);
-  }
+    atualizar(id: number, paciente: Partial<Omit<PacienteAtualizar, 'id'>>) {
+        return this.http.put<PacienteAtualizar>(API_ROUTES.pacientes.atualizar(id), paciente);
+    }
 
-  remover(id: number) {
-    return this.http.delete<void>(API_ROUTES.pacientes.remover(id));
-  }
+    remover(id: number) {
+        return this.http.delete<void>(API_ROUTES.pacientes.remover(id));
+    }
 }
